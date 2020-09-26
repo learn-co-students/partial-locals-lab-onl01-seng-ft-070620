aa10bae3739c4d6ac9c651aaa53f8801837f8ebe
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-09-26 16:07:25 -0500

    Done.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: classrooms
@@ -14,6 +16,6 @@ class Classroom < ActiveRecord::Base
   has_many :students, through: :classroom_students
 
   def oldest_student
-    students.where("birthday is not null").order("birthday asc").first
+    students.where('birthday is not null').order('birthday asc').first
   end
 end
--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StudentsController, type: :controller do
@@ -7,16 +9,15 @@ RSpec.describe StudentsController, type: :controller do
 
       it 'only renders items that match the query' do
         b = Faker::Date.between(from: 25.years.ago, to: 18.years.ago)
-        Student.create(name: 'Byronius Karbitus Maris', hometown: "San Francisco", birthday: b)
-        Student.create(name: 'Notorious Byron', hometown: "Brooklyn", birthday: b)
-        Student.create(name: 'Blah Byron Blah', hometown: "Manhattan", birthday: b)
-        Student.create(name: 'King Zebra', hometown: "New Prussia", birthday: b)
+        Student.create(name: 'Byronius Karbitus Maris', hometown: 'San Francisco', birthday: b)
+        Student.create(name: 'Notorious Byron', hometown: 'Brooklyn', birthday: b)
+        Student.create(name: 'Blah Byron Blah', hometown: 'Manhattan', birthday: b)
+        Student.create(name: 'King Zebra', hometown: 'New Prussia', birthday: b)
 
-        get :index, params: { query: "Byron" }
+        get :index, params: { query: 'Byron' }
 
         expect(response.body).not_to match(/Zebra/)
       end
     end
   end
-
 end
